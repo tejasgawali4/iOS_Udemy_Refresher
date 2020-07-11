1294912147fca222ddb80314a79df50243f34dce
tejasgawali4 <tejas.gawali4@gmail.com>
2020-07-11 20:43:45 +0530

    i am rich

---
+++ IAmRIch/I Am Rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by EAG on 7/11/20.
+//  Copyright © 2020 Synechron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var a = 1;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
