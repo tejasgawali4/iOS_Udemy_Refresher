ca6095a9298be8f2c6aaccd5ea7dd6ce6973e38d
Tejas Gawali <tejasgawali4@users.noreply.github.com>
2020-07-11 23:20:03 +0530

    Update ViewController.swift

--- IAmRIch/I Am Rich/ViewController.swift
+++ IAmRIch/I Am Rich/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  I Am Rich
-//
-//  Created by EAG on 7/11/20.
-//  Copyright © 2020 Synechron. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
